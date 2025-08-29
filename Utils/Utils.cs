312b5f558c0601fd5e043e13eeebb457826dd75d
Dukisden <letrucbleu@gmail.com>
2025-08-29 02:55:37 +0200

    project init

---
+++ Utils/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Library;
+
+namespace DK_Collection.Utils
+{
+    internal class Utils
+    {
+        public static void Log(string message, bool isError = false)
+        {
+            var color = new Color(0.333f, 0.612f, 0.839f);
+            if (isError)
+            {
+                color = new Color(0.9f, 0f, 0f);
+            }
+            InformationManager.DisplayMessage(new InformationMessage(message, color));
+        }
+
+        public static float Clamp(float value, float min, float max)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
+    }
+}
