7577d6ba64c902712c68caed151e709bb1d9bc2c
Dukisden <letrucbleu@gmail.com>
2025-09-01 05:41:06 +0200

    change default settings

--- MCMSettings.cs
+++ MCMSettings.cs
@@ -12,33 +12,29 @@ namespace DukisCollection
         public override string FolderName => "DukisCollection";
         public override string FormatType => "json2";
 
+        // Bleed
         [SettingPropertyBool("Enable Bleed", Order = 0, IsToggle = true, RequireRestart = false, HintText = "When enabled, strikes will have a chance to cause bleed over time. Blades cause more bleed. Hits to the head/torso cause more bleed. High damage hits cause more bleed. Healthy and armored units are less likely to bleed.")]
         [SettingPropertyGroup("Bleed")]
-        public bool EnableBleed { get; set; } = true;
+        public bool EnableBleed { get; set; } = false;
 
         [SettingPropertyBool("Proc info debug", Order = 0, RequireRestart = false, HintText = "Enable to display bleed proc chance detail on hit")]
         [SettingPropertyGroup("Bleed")]
         public bool BleedDebug { get; set; } = false;
 
-
+        // Tourney
         [SettingPropertyBool("Enable Partial Tourney Reward", Order = 0, IsToggle = true, RequireRestart = false, HintText = "When enabled, losing a tourney will still reward some gold based on round reached.")]
         [SettingPropertyGroup("Partial Tourney Reward")]
-        public bool EnableTourney { get; set; } = true;
+        public bool EnableTourney { get; set; } = false;
 
-
+        // Damage
         [SettingPropertyBool("Enable Damage mods", Order = 0, IsToggle = true, RequireRestart = false, HintText = "When enabled, blunt damage is less affected by armor. Cut damage is more affected by armor. Axes do more damage to shields.")]
         [SettingPropertyGroup("Type Damage Modifiers")]
-        public bool EnableDamage { get; set; } = true;
+        public bool EnableDamage { get; set; } = false;
 
-
-        [SettingPropertyBool("Enable Preserve Companions", Order = 0, IsToggle = true, RequireRestart = false, HintText = "When enabled, there will be a prompt on companion death to preserve them as a dead member of your clan.")]
-        [SettingPropertyGroup("Preserve Companions")]
-        public bool EnableCompanions { get; set; } = true;
-
-
-        [SettingPropertyBool("Enable More Deaths", Order = 1, IsToggle = true, RequireRestart = false, HintText = "When enabled the player, lords and companions are more likely to die on the battlefield.")]
+        // Death
+        [SettingPropertyBool("Enable More Deaths", Order = 0, IsToggle = true, RequireRestart = false, HintText = "When enabled the player, lords and companions are more likely to die on the battlefield.")]
         [SettingPropertyGroup("More Hero Deaths")]
-        public bool EnableDeath { get; set; } = true;
+        public bool EnableDeath { get; set; } = false;
 
         [SettingPropertyFloatingInteger("Death chance multiplier", 0, 100, Order = 1, RequireRestart = false, HintText = "0 = no deaths, 1 = no change, 10 = ten times more likely to die. Applies to Player, Companions & Lords.")]
         [SettingPropertyGroup("More Hero Deaths", GroupOrder = 1)]
@@ -48,7 +44,12 @@ namespace DukisCollection
         [SettingPropertyGroup("More Hero Deaths", GroupOrder = 1)]
         public bool DeathDebug { get; set; } = false;
 
+        // Companions
+        [SettingPropertyBool("Enable Preserve Companions", Order = 1, IsToggle = true, RequireRestart = false, HintText = "When enabled, there will be a prompt on companion death to preserve them as a dead member of your clan.")]
+        [SettingPropertyGroup("Preserve Companions")]
+        public bool EnableCompanions { get; set; } = true;
 
+        // Toolbox
         [SettingPropertyGroup("Toolbox", GroupOrder = 99)]
         [SettingPropertyButton("Force assault on player siege", Order = 0, RequireRestart = false, HintText = "When the player is defending or attacking a settlement, force the besieging AI to launch the assault.", Content = "Force Assault")]
         public Action ForceAssault { get; set; } = delegate ()
